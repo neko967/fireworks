b5ce0afaca8560c8212b189feec7f659e886ea91
neko967 <113199838+neko967@users.noreply.github.com>
2023-08-14 01:34:34 +0900

    Fix: カウンターを修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,23 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+console.log("application.jsを読み込見ました");
 import "@hotwired/turbo-rails"
 import "controllers"
-import "staticpages/top"
-
-document.addEventListener('DOMContentLoaded', function() {
-  function setup() {
-    // キャンバスの設定
-    background(0);
-    let canvas = document.getElementById('firework');
-    let ctx = canvas.getContext('2d');
-    canvas = createCanvas(480, 480);
-    canvas.position(0, 0);
-    canvas.style("z-index", "-1");
-    colorMode(RGB);
-    frameRate(60);
-  }
-  function draw() {
-    background(200);
-    color(255);
-    ellipse(240, 240, 20, 20);
-  }
-});

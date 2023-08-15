bbd220365d0c0ce969e67be59950b319c6fd4241
neko967 <113199838+neko967@users.noreply.github.com>
2023-08-16 02:18:52 +0900

    Delete: 不要なclicks_controllerを削除

--- app/controllers/clicks_controller.rb
+++
@@ -1,7 +0,0 @@
-class ClicksController < ApplicationController
-  def create
-    click_count = params[:click_count].to_i
-    Click.create(count: click_count)
-    render json: { message: 'Click count saved successfully' }
-  end
-end
